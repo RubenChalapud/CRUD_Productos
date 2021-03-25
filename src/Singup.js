e55e60dded4d13b02db792a152a86ce49615e8f5
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2021-03-25 10:45:33 -0500

    Pagina de registro creada usando Material-Ui

--- src/Singup.js
+++ src/Singup.js
@@ -1,26 +1,117 @@
-import React, { useState } from "react";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 import "./Login.css"
 import MenssageContainer from "./Components/Login/Menssage/MenssageContainer";
 
-function Singup(){
-    return(
-        <div className="o-container-login">
-            <MenssageContainer></MenssageContainer>
-            <div className="o-container-log-form">
-                <h1>Registrarse</h1>
-                <h4>Para registrarse deberas introducir tu e-mail registrado y una contraseña</h4>
-                <div className="o-container-input-text">
-                    <div className="o-inputcolor"></div>
-                    <input type="text" className="o-input-text"></input>
-                </div>
-                <div className="o-container-input-text">
-                    <div className="o-inputcolor"></div>
-                    <input type="text" className="o-input-text"></input>
-                </div>
-                <button>Registrarse</button>
-            </div>
-        </div>
-    );
-}
 
-export default Singup;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <div className="o-container-login">
+        <MenssageContainer></MenssageContainer>
+        <div className="o-container-singup">
+                <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                    Registrarse
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                        <TextField
+                            autoComplete="fname"
+                            name="Nombre"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="firstName"
+                            label="Nombre"
+                            autoFocus
+                        />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="lastName"
+                            label="Apellido"
+                            name="Apellido"
+                            autoComplete="lname"
+                        />
+                        </Grid>
+                        <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Correo Electrónico"
+                            name="Email"
+                            autoComplete="email"
+                        />
+                        </Grid>
+                        <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="Contraseña"
+                            label="Contraseña"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Registrarse
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                        <Link href="/" variant="body2">
+                            ¿Ya estas registrado? Ingresa aquí
+                        </Link>
+                        </Grid>
+                    </Grid>
+                    </form>
+                </div>
+            </Container>
+        </div>
+        
+    </div>
+    
+  );
+}
